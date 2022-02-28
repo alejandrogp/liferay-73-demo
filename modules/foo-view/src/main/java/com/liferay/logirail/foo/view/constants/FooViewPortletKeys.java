339023dbf3c1c486d362d320d8fe98817e8bdc91
A. García <alejandrogp@gmail.com>
2022-02-28 18:12:46 +0100

    Crear modulo MVC

---
+++ modules/foo-view/src/main/java/com/liferay/logirail/foo/view/constants/FooViewPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.logirail.foo.view.constants;
+
+/**
+ * @author L71877487A
+ */
+public class FooViewPortletKeys {
+
+	public static final String FOOVIEW =
+		"com_liferay_logirail_foo_view_FooViewPortlet";
+
+}
