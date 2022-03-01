5bd317851bd8c4f58435ddd546d54c89063c83dd
A. García <alejandrogp@gmail.com>
2022-03-01 13:05:02 +0100

    Operaciones CRUD

--- modules/foo-view/src/main/java/com/liferay/logirail/foo/view/constants/FooViewPortletKeys.java
+++ modules/foo-view/src/main/java/com/liferay/logirail/foo/view/constants/FooViewPortletKeys.java
@@ -8,4 +8,11 @@ public class FooViewPortletKeys {
 	public static final String FOOVIEW =
 		"com_liferay_logirail_foo_view_FooViewPortlet";
 
+	public static final String FOO_EDIT_ID = "fooEditId";
+
+	public static final String FIELD1 = "field1";
+	public static final String FIELD2 = "field2";
+	public static final String FIELD3 = "field3";
+	public static final String FIELD4 = "field4";
+	public static final String FIELD5 = "field5";
 }
